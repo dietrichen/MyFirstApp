3f1b4bd664bca400e888a17ecba1bfa57dd1dce1
dietrichen <ed819118@wcupa.edu>
2016-12-14 17:56:29 -0500

    Update code for emotion api

--- app/src/main/java/com/contoso/myfirstapp/MainActivity.java
+++ app/src/main/java/com/contoso/myfirstapp/MainActivity.java
@@ -1,8 +1,11 @@
+
 package com.contoso.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import java.io.*;
+
 import android.app.*;
 import android.content.*;
 import android.net.*;
@@ -12,20 +15,32 @@ import android.graphics.*;
 import android.widget.*;
 import android.provider.*;
 
-import com.microsoft.projectoxford.face.*;
-import com.microsoft.projectoxford.face.contract.*;
+import java.util.List;
+
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Face;
+import com.microsoft.projectoxford.emotion.EmotionServiceClient;
+import com.microsoft.projectoxford.emotion.EmotionServiceRestClient;
+import com.microsoft.projectoxford.emotion.contract.FaceRectangle;
+import com.microsoft.projectoxford.emotion.contract.RecognizeResult;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private final int PICK_IMAGE = 1;
     private ProgressDialog detectionProgressDialog;
     private FaceServiceClient faceServiceClient =
             new FaceServiceRestClient("3289657d188b4159b39df028555c5a19");
+    private EmotionServiceClient emotionServiceClient =
+            new EmotionServiceRestClient("dad2de94aee4433d82c6b0a794e3c9c4");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
+        Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         detectionProgressDialog = new ProgressDialog(this);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -48,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setImageBitmap(bitmap);
 
                 detectAndFrame(bitmap);
+                //detectEmotion(bitmap);
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -56,74 +73,88 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Detect faces by uploading face images
-// Frame faces after detection
+    // Frame faces after detection
 
-    private void detectAndFrame(final Bitmap imageBitmap)
-    {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private void detectAndFrame(final Bitmap imageBitmap) {
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-        ByteArrayInputStream inputStream =
+        final ByteArrayInputStream inputStream =
                 new ByteArrayInputStream(outputStream.toByteArray());
-        AsyncTask<InputStream, String, Face[]> detectTask =
-                new AsyncTask<InputStream, String, Face[]>() {
+        AsyncTask<InputStream, String, List<RecognizeResult>> detectTask =
+                new AsyncTask<InputStream, String, List<RecognizeResult>>() {
                     @Override
-                    protected Face[] doInBackground(InputStream... params) {
+                    protected List<RecognizeResult> doInBackground(InputStream... params) {
                         try {
                             publishProgress("Detecting...");
                             Face[] result = faceServiceClient.detect(
                                     params[0],
                                     true,         // returnFaceId
                                     false,        // returnFaceLandmarks
-                                    null           // returnFaceAttributes: a string like "age, gender"
+                                    null          // returnFaceAttributes: a string like "age, gender"
                             );
-                            if (result == null)
-                            {
+                            if (result == null) {
                                 publishProgress("Detection Finished. Nothing detected");
                                 return null;
                             }
                             publishProgress(
                                     String.format("Detection Finished. %d face(s) detected",
                                             result.length));
-                            return result;
+
+                            com.microsoft.projectoxford.emotion.contract.FaceRectangle[] faceRectangles =
+                                    new com.microsoft.projectoxford.emotion.contract.FaceRectangle[result.length];
+                            for (int i = 0; i < result.length; i++) {
+                                com.microsoft.projectoxford.face.contract.FaceRectangle tempRectangles = result[i].faceRectangle;
+                                faceRectangles[i] = new com.microsoft.projectoxford.emotion.contract.FaceRectangle(tempRectangles.left,
+                                        tempRectangles.top, tempRectangles.width, tempRectangles.height);
+                            }
+                            List<RecognizeResult> finalFaces;
+                            inputStream.reset();
+                            finalFaces = emotionServiceClient.recognizeImage(inputStream, faceRectangles);
+                            return finalFaces;
+
                         } catch (Exception e) {
                             publishProgress("Detection failed");
                             return null;
                         }
                     }
+
                     @Override
                     protected void onPreExecute() {
 
                         detectionProgressDialog.show();
                     }
+
                     @Override
                     protected void onProgressUpdate(String... progress) {
 
                         detectionProgressDialog.setMessage(progress[0]);
                     }
+
                     @Override
-                    protected void onPostExecute(Face[] result) {
+                    protected void onPostExecute(List<RecognizeResult> result) {
 
                         detectionProgressDialog.dismiss();
                         if (result == null) return;
-                        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
                         imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
                         imageBitmap.recycle();
                     }
                 };
         detectTask.execute(inputStream);
     }
-    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+
+    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, List<RecognizeResult> faces) {
         Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.RED);
+        paint.setColor(Color.BLUE);
         int stokeWidth = 2;
         paint.setStrokeWidth(stokeWidth);
         if (faces != null) {
-            for (Face face : faces) {
-                FaceRectangle faceRectangle = face.faceRectangle;
+            for (RecognizeResult currResult : faces) {
+                FaceRectangle faceRectangle = currResult.faceRectangle;
                 canvas.drawRect(
                         faceRectangle.left,
                         faceRectangle.top,
@@ -134,5 +165,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return bitmap;
     }
-
 }
